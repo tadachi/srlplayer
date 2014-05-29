5c3d97b5d4c6a16a0acb23c434118b3fbc04a591
Takumi Adachi <takumiadachi@gmail.com>
2014-05-29 01:52:25 -0700

    - Improved the layout. - Greately improved the left panel with twitch streams. - Hide left and right panel works on all browsers (IE, FF, CRM). - Centerpanel with player automatically resizes hide/show left and right.

--- server3.js
+++ server3.js
@@ -5,6 +5,7 @@ var app 		= express()
 var port		= parseInt(process.env.PORT, 10) || 4000;
 var fs 			= require('fs');
 var path		= require('path');
+
 /*
 app.get("/", function(req, res) {
   res.redirect("/index.html");
