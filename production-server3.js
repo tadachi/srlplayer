0f130d61c378d22b23ec8b4a687a2b011799c75f
Takumi Adachi <takumiadachi@gmail.com>
2014-06-02 14:54:35 -0700

    nodejs server uses vhosts now.

---
+++ production-server3.js
@@ -0,0 +1,30 @@
+var express 	= require('express');
+var bodyParser 	= require('body-parser');
+var methodOverride = require('method-override');
+var app 		= express()
+var port		= parseInt(process.env.PORT, 10) || 80;
+var fs 			= require('fs');
+var path		= require('path');
+var router 		= express.Router();
+var vhost = require('vhost');
+
+var srl = express();
+var home = express();
+
+app.use(bodyParser());
+app.use(methodOverride());
+
+
+
+srl.use(express.static(__dirname + '\\app\\'));		//srl
+//home.use(express.static(__dirname)); 				//homepage
+
+app.use(vhost('srl.takbytes.com', srl));
+//app.use(vhost('www.takbytes.com', home));
+
+app.listen(port);
+
+/* Debug */
+console.log(__dirname);
+console.log(__dirname + '\\app\\');
+console.log('Listening on port: ' + port);
