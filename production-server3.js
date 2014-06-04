cc918f9edfb5ae6847e71ecb89b76eb776ae5d47
Takumi Adachi <takumiadachi@gmail.com>
2014-06-04 09:30:25 -0700

    Fixed an issue within srlTeamChannelTable.

--- production-server3.js
+++ production-server3.js
@@ -2,7 +2,7 @@ var express 	= require('express');
 var bodyParser 	= require('body-parser');
 var methodOverride = require('method-override');
 var app 		= express()
-var port		= parseInt(process.env.PORT, 10) || 80;
+var port		= parseInt(process.env.PORT, 10) || 4000;
 var fs 			= require('fs');
 var path		= require('path');
 var router 		= express.Router();
@@ -14,12 +14,11 @@ var home = express();
 app.use(bodyParser());
 app.use(methodOverride());
 
-
-
 srl.use(express.static(__dirname + '\\app\\'));		//srl
 //home.use(express.static(__dirname)); 				//homepage
 
 app.use(vhost('srl.takbytes.com', srl));
+app.use(vhost('srl.tak.com', srl));
 //app.use(vhost('www.takbytes.com', home));
 
 app.listen(port);
